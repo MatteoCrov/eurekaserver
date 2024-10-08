fe78769ffa5915607063e5e4e7db6329f31578bf
LeonardoFagnani-dev <leofagnani.00@gmail.com>
2024-10-08 11:20:11 +0200

    yml and dependecies

--- src/main/java/eu/pg/eurekaserver/EurkaserverApplication.java
+++ src/main/java/eu/pg/eurekaserver/EurkaserverApplication.java
@@ -2,8 +2,10 @@ package eu.pg.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurkaserverApplication {
 
 	public static void main(String[] args) {
